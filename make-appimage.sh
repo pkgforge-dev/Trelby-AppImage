ea97e9bfc1ea8a414ed5bd505d4b4d9587c3b884
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-23 02:55:12 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q trelby | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/trelby/resources/icon256.png
+export DESKTOP=/usr/share/applications/trelby.desktop
+export DEPLOY_PYTHON=1
+export ALWAYS_SOFTWARE=1 # gtk3 is not hardware accelerated
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/trelby
 
 # Additional changes can be done in between here
 

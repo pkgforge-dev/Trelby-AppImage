6e32b00de6f70eafab2b79a5ad678787042a4c1a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-23 03:32:30 -0400

    fix weird issue

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,6 +15,11 @@ get-debloated-pkgs --add-common --prefer-nano
 # Comment this out if you need an AUR package
 make-aur-package trelby
 
+# for some reason when xvfb-run is used in quick-sharun the app somehow
+# ends up dlopening mesa, which makes no sense at all, this does not happen when
+# deploying locally, and I can reproduce the issue once I try with xvfb-run
+pacman -Rdd --noconfirm mesa
+
 # If the application needs to be manually built that has to be done down here
 
 # if you also have to make nightly releases check for DEVEL_RELEASE = 1
